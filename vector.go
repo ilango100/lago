e98bf9a014ee9f3d46c09170629d9f9aef8f60cd
Ilango R <ilangokul@gmail.com>
2019-04-12 14:20:29 +0530

    Vector slicing

--- vector.go
+++ vector.go
@@ -27,3 +27,32 @@ func (v Vector) String() (s string) {
 	s += "\b\b ]"
 	return s
 }
+
+//Slice slices vector from i to j
+func (v Vector) Slice(arr ...int) (Vector, error) {
+	i, j, k := 0, v.n, 1
+	switch len(arr) {
+	case 0:
+	case 1:
+		j = arr[0]
+	default:
+		k = arr[2]
+		fallthrough
+	case 2:
+		i, j = arr[0], arr[1]
+	}
+	if i >= j {
+		return v, fmt.Errorf("i >= j")
+	}
+	if i > v.n || j > v.n {
+		return v, fmt.Errorf("index > v.n")
+	}
+	v.data = v.data[i*v.inc:]
+	//v.inc = v.inc*step + step + v.inc
+	v.inc = v.inc * k
+	v.n = (j - i) / k
+	if (j-i)%k > 0 {
+		v.n++
+	}
+	return v, nil
+}
--- vector_test.go
+++ vector_test.go
@@ -7,7 +7,20 @@ func TestVectorString(t *testing.T) {
 	res := "[ 1, 2, 3, 4, 5, \b\b ]"
 	str := v.String()
 	if str != res {
-		t.Errorf("Error converting to string: '%s' %s '%s'", str, "!=", res)
+		t.Errorf("Error converting to string: '%s' != '%s'", str, res)
 	}
 	t.Logf("Vector string: %s", str)
+}
+
+func TestVectorSlice(t *testing.T) {
+	v := NewVector([]float64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
+	res := "[ 1, 4, \b\b ]"
+	slc, err := v.Slice(1, 5, 3)
+	if err != nil {
+		t.Errorf("Error slicing: %v", err)
+	}
+	if str := slc.String(); str != res {
+		t.Errorf("Sliced string: %s != %s ", str, res)
+	}
+	t.Logf("Sliced vector: %s", slc.String())
 }
